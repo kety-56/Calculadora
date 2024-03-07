e6ab3999753ed94bd962ee59f72a7a8e19b9f1d1
kety-56 <britolketlyn@gmail.com>
2024-03-07 19:54:25 -0300

    Exercicio S06

---
+++ calculadora.js
@@ -0,0 +1,5 @@
+let numeroA = prompt("Insira o primeiro número");
+let numeroB = prompt("Insira o segundo número");
+let operacao = prompt("Insira a operação desejada");
+
+window.alert ("A soma dos némeros é: " + eval(numeroA + operacao + numeroB))
